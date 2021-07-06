ab479fb194999edaabd02cf3604fcfa94d3a9ee9
Joseph Hopkins <joseph.r.hopkins88@gmail.com>
2021-07-06 13:53:23 -0500

    update

---
+++ src/main/java/Album.java
@@ -0,0 +1,72 @@
+import java.io.Serializable;
+
+public class Album implements Serializable {
+    private long id;
+    private String author;
+    private String name;
+    private int release_date;
+    private double sales;
+    private String genre;
+
+    public Album() {
+    }
+
+    public Album(long id, String author, String name, int release_date, double sales, String genre) {
+        this.id = id;
+        this.author = author;
+        this.name = name;
+        this.release_date = release_date;
+        this.sales = sales;
+        this.genre = genre;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRelease_date() {
+        return release_date;
+    }
+
+    public void setRelease_date(int release_date) {
+        this.release_date = release_date;
+    }
+
+    public double getSales() {
+        return sales;
+    }
+
+    public void setSales(double sales) {
+        this.sales = sales;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+
+}
